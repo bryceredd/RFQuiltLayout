3eedf7dab59d22501f5789ff11d5bc3c7507b716
Antoine Lamy <alamy@lapresse.ca>
2014-03-27 13:32:56 -0400

    Basic demo update to demonstrate the sections header/footer

--- QuiltDemo/RFViewController.m
+++ QuiltDemo/RFViewController.m
@@ -13,23 +13,38 @@
     BOOL isAnimating;
 }
 @property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
-@property (nonatomic) NSMutableArray* numbers;
+@property (nonatomic) NSMutableArray* sections;
 @end
-
-int num = 0;
 
 @implementation RFViewController
 
 - (void)viewDidLoad {
     
-    self.numbers = [@[] mutableCopy];
-    for(; num<100; num++) { [self.numbers addObject:@(num)]; }
+	self.sections = [NSMutableArray array];
+	
+	int num = 0;
+	for(int i=0; i < 8; i++)
+	{
+		NSMutableArray *section = [NSMutableArray array];
+		for(int j=0; j < 28; j++)
+		{
+			[section addObject:@(num)];
+			num ++;
+		}
+		[self.sections addObject:section];
+	}
     
     [self.collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:@"cell"];
+	[self.collectionView registerClass:[UICollectionReusableView class] forSupplementaryViewOfKind:UICollectionElementKindSectionHeader withReuseIdentifier:@"headerView"];
+	[self.collectionView registerClass:[UICollectionReusableView class] forSupplementaryViewOfKind:UICollectionElementKindSectionFooter withReuseIdentifier:@"footerView"];
     
     RFQuiltLayout* layout = (id)[self.collectionView collectionViewLayout];
     layout.scrollDirection = UICollectionViewScrollDirectionVertical;
     layout.itemBlockSize = CGSizeMake(100, 100);
+	layout.sectionInset = UIEdgeInsetsMake(10, 150, 50, 150);
+	layout.itemInset = UIEdgeInsetsMake(4, 4, 4, 4);
+	layout.headerReferenceSize = CGSizeMake(720, 70);
+	layout.footerReferenceSize = CGSizeMake(720, 70);
     
     [self.collectionView reloadData];
 }
@@ -39,18 +54,18 @@ int num = 0;
 }
 
 - (IBAction)remove:(id)sender {
-    if(!self.numbers.count) return;
-    
-    if(isAnimating) return;
-    isAnimating = YES;
-    
-    [self.collectionView performBatchUpdates:^{
-        int index = arc4random() % MAX(1, self.numbers.count);
-        [self.numbers removeObjectAtIndex:index];
-        [self.collectionView deleteItemsAtIndexPaths:@[[NSIndexPath indexPathForRow:index inSection:0]]];
-    } completion:^(BOOL done) {
-        isAnimating = NO;
-    }];
+	//    if(!self.numbers.count) return;
+	//
+	//    if(isAnimating) return;
+	//    isAnimating = YES;
+	//
+	//    [self.collectionView performBatchUpdates:^{
+	//        int index = arc4random() % MAX(1, self.numbers.count);
+	//        [self.numbers removeObjectAtIndex:index];
+	//        [self.collectionView deleteItemsAtIndexPaths:@[[NSIndexPath indexPathForRow:index inSection:0]]];
+	//    } completion:^(BOOL done) {
+	//        isAnimating = NO;
+	//    }];
 }
 
 - (IBAction)refresh:(id)sender {
@@ -58,69 +73,81 @@ int num = 0;
 }
 
 - (IBAction)add:(id)sender {
-    if(isAnimating) return;
-    isAnimating = YES;
-	
-    [self.collectionView performBatchUpdates:^{
-        int index = arc4random() % MAX(self.numbers.count,1);
-        [self.numbers insertObject:@(++num) atIndex:index];
-        [self.collectionView insertItemsAtIndexPaths:@[[NSIndexPath indexPathForRow:index inSection:0]]];
-    } completion:^(BOOL done) {
-        isAnimating = NO;
-    }];
+	//    if(isAnimating) return;
+	//    isAnimating = YES;
+	//
+	//    [self.collectionView performBatchUpdates:^{
+	//        int index = arc4random() % MAX(self.numbers.count,1);
+	//        [self.numbers insertObject:@(++num) atIndex:index];
+	//        [self.collectionView insertItemsAtIndexPaths:@[[NSIndexPath indexPathForRow:index inSection:0]]];
+	//    } completion:^(BOOL done) {
+	//        isAnimating = NO;
+	//    }];
 }
 
-- (UIColor*) colorForNumber:(NSNumber*)num {
+- (UIColor*)colorForNumber:(NSNumber*)num {
     return [UIColor colorWithHue:((19 * num.intValue) % 255)/255.f saturation:1.f brightness:1.f alpha:1.f];
+}
+
+- (CGSize)blockSizeForNumber:(NSNumber*)num {
+	return ([num intValue] % 2 ? CGSizeMake(1, 1) : CGSizeMake(2, 1));
 }
 
 #pragma mark - UICollectionView Datasource
 
+- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView
+{
+	return [self.sections count];
+}
+
 - (NSInteger)collectionView:(UICollectionView *)view numberOfItemsInSection:(NSInteger)section {
-    return self.numbers.count;
+	return [[self.sections objectAtIndex:section] count];
 }
 
 - (UICollectionViewCell *)collectionView:(UICollectionView *)cv cellForItemAtIndexPath:(NSIndexPath *)indexPath {
     UICollectionViewCell *cell = [cv dequeueReusableCellWithReuseIdentifier:@"cell" forIndexPath:indexPath];
-    cell.backgroundColor = [self colorForNumber:self.numbers[indexPath.row]];
     
     UILabel* label = (id)[cell viewWithTag:5];
-    if(!label) label = [[UILabel alloc] initWithFrame:CGRectMake(50, 50, 30, 20)];
-    label.tag = 5;
-    label.textColor = [UIColor blackColor];
-    label.text = [NSString stringWithFormat:@"%@", self.numbers[indexPath.row]];
-    label.backgroundColor = [UIColor clearColor];
-    [cell addSubview:label];
-    
+    if(!label) {
+		label = [[UILabel alloc] initWithFrame:CGRectMake(50, 50, 30, 20)];
+		label.tag = 5;
+		label.textColor = [UIColor blackColor];
+		label.backgroundColor = [UIColor clearColor];
+		[cell addSubview:label];
+	}
+	
+	NSNumber *number = [[self.sections objectAtIndex:indexPath.section] objectAtIndex:indexPath.row];
+	cell.backgroundColor = [self colorForNumber:number];
+	label.text = [NSString stringWithFormat:@"%@", number];
+	
     return cell;
+}
+
+- (UICollectionReusableView *)collectionView:(UICollectionView *)collectionView viewForSupplementaryElementOfKind:(NSString *)kind atIndexPath:(NSIndexPath *)indexPath
+{
+	UICollectionReusableView *view = nil;
+    if ([kind isEqualToString:UICollectionElementKindSectionHeader])
+	{
+        UICollectionReusableView *headerView = [collectionView dequeueReusableSupplementaryViewOfKind:UICollectionElementKindSectionHeader withReuseIdentifier:@"headerView" forIndexPath:indexPath];
+		headerView.backgroundColor = [UIColor redColor];
+        view = headerView;
+    }
+	else if ([kind isEqualToString:UICollectionElementKindSectionFooter])
+	{
+        UICollectionReusableView *footerView = [collectionView dequeueReusableSupplementaryViewOfKind:UICollectionElementKindSectionFooter withReuseIdentifier:@"footerView" forIndexPath:indexPath];
+		footerView.backgroundColor = [UIColor blueColor];
+        view = footerView;
+    }
+	
+    return view;
 }
 
 
 #pragma mark – RFQuiltLayoutDelegate
 
 - (CGSize) blockSizeForItemAtIndexPath:(NSIndexPath *)indexPath {
-    
-    if(indexPath.row >= self.numbers.count)
-        NSLog(@"Asking for index paths of non-existant cells!! %d from %d cells", indexPath.row, self.numbers.count);
-    
-    if (indexPath.row % 10 == 0)
-        return CGSizeMake(3, 1);
-    if (indexPath.row % 11 == 0)
-        return CGSizeMake(2, 1);
-    else if (indexPath.row % 7 == 0)
-        return CGSizeMake(1, 3);
-    else if (indexPath.row % 8 == 0)
-        return CGSizeMake(1, 2);
-    else if(indexPath.row % 11 == 0)
-        return CGSizeMake(2, 2);
-    if (indexPath.row == 0) return CGSizeMake(5, 5);
-    
-    return CGSizeMake(1, 1);
+	NSNumber *number = [[self.sections objectAtIndex:indexPath.section] objectAtIndex:indexPath.row];
+	return [self blockSizeForNumber:number];
 }
-
-- (UIEdgeInsets)insetsForItemAtIndexPath:(NSIndexPath *)indexPath {
-    return UIEdgeInsetsMake(2, 2, 2, 2);
-}
-
 
 @end
